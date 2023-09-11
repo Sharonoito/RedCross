cdf482d51edce5b2deefce9b799ddb8bbb0ed1cd
dvambrose <ambuhmwangi.code@gmil.com>
2023-09-11 13:37:53 +0300

    fix for connectionstrng

--- RedCrossChat/RedCrossChat/Startup.cs
+++ RedCrossChat/RedCrossChat/Startup.cs
@@ -91,7 +91,7 @@ namespace RedCrossChat
             // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
             services.AddSingleton<IStorage, MemoryStorage>();
 
-            var connectionString = _config.GetConnectionString("DefaultConnection");
+            var connectionString = _config.GetConnectionString("LocalConnection");
 
             //Use sql Server Conversations
             services.AddDbContextPool<AppDBContext>(options =>
