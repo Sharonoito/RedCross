c9ba3959929d0a023602730971105908b506d43d
Sharon <oitosharonakoth@gmail.com>
2023-11-03 11:11:24 +0300

    feedback

--- RedCrossChat/RedCrossChat/ViewModel/AgeBandVm.cs
+++ RedCrossChat/RedCrossChat/ViewModel/AgeBandVm.cs
@@ -4,7 +4,7 @@ namespace RedCrossChat
 {
     public class AgeBandVM
     {
-        public Guid Id { get; set; }
+        public Guid Id {  get; set; }   
 
         public string Name { get; set; }
         public string Kiswahili { get; set; }
